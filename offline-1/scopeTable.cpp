8a242ee5940f6d20566ec054f2cda782f2a41625
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2025-04-17 15:01:25 +0600

    des.txt

--- offline-1/scopeTable.cpp
+++ offline-1/scopeTable.cpp
@@ -6,11 +6,62 @@ private:
     int sid;
     int bucketSize ;
     int scopeCount ;
-    unsigned long (*hashFunction)(const std::string&); // function pointer
-    symbolInfo** table; // linked list of symbolInfo
+    unsigned long (*hashFunction)(const string&); // function pointer
+    symbolInfo** hashtable; // hashtable of symbolInfo
     ScopeTable* parentScope ; // pointer to parent scope, to be used as a stack
 public:
+    ScopeTable(int bucketSize, int sid, unsigned long (*hashFunction)(const string&)) : bucketSize(bucketSize), sid(sid), hashFunction(hashFunction) {
+        hashtable = new symbolInfo*[bucketSize];
+        for (int i = 0; i < bucketSize; i++) {
+            hashtable[i] = nullptr;
+        }
+        scopeCount = 0;
+        parentScope = nullptr;
+    }
 
+    ~ScopeTable() {
+        for (int i = 0; i < bucketSize; i++) {
+            delete hashtable[i];
+        }
+        delete[] hashtable;
+    }
 
+    void setParentScope(ScopeTable* parent) {
+        parentScope = parent;
+        if (parent != nullptr) parent->scopeCount++;
+    }
+
+    ScopeTable* getParentScope() {
+        return parentScope;
+    }
+
+    int getId() {
+        return sid;
+    }
+
+    int getBucketSize() {
+        return bucketSize;
+    }
+
+    int getScopeCount() {
+        return scopeCount;
+    }
+
+    bool InsertSymbol(symbolInfo * sInfo) {
+        unsigned long index = hashFunction(sInfo->getName());
+        if (hashtable[index] == nullptr) {
+            hashtable[index] = sInfo;
+            scopeCount++;
+            return true;
+        } else {
+            symbolInfo* current = hashtable[index];
+            while (current->getNextSymbol() != nullptr) {
+                current = current->getNextSymbol();
+            }
+            current->setNextSymbol(sInfo);
+            scopeCount++;
+            return true;
+        }
+    }
 
 };
--- offline-1/test.cpp
+++ offline-1/test.cpp
@@ -1,4 +1,4 @@
-#include"symbolInfo.h"
+#include"scopeTable.cpp"
 using namespace std;
 
 int main() {
@@ -15,6 +15,34 @@ int main() {
     s2.setType("double");
     cout << "After modification, s2: " << s2 << endl; // Output: Name: z, Type: double
     cout << "Next Symbol of s2 after modification: " << *(s2.getNextSymbol()) << endl; // Output: Name: x, Type: int
+
+    ScopeTable scopeTable(10, 1, [](const string& name) {
+        unsigned long hash = 5381;
+        for (char c : name) {
+            hash = ((hash << 5) + hash) + c; // hash * 33 + c
+        }
+        return hash % 10; // Assuming bucket size is 10
+    });
+    scopeTable.setParentScope(nullptr);
+    cout << "ScopeTable ID: " << scopeTable.getId() << endl; // Output: ScopeTable ID: 1
+    cout << "ScopeTable Bucket Size: " << scopeTable.getBucketSize() << endl; // Output: ScopeTable Bucket Size: 10
+    cout << "ScopeTable Scope Count: " << scopeTable.getScopeCount() << endl; // Output: ScopeTable Scope Count: 0
+    cout << "Parent Scope: " << scopeTable.getParentScope() << endl; // Output: Parent Scope: 0x0 (or nullptr)
+    
+    ScopeTable parentScope(5, 0, [](const string& name) {
+        unsigned long hash = 5381;
+        for (char c : name) {
+            hash = ((hash << 5) + hash) + c; // hash * 33 + c
+        }
+        return hash % 5; // Assuming bucket size is 5
+    });
+    parentScope.setParentScope(nullptr);
+    scopeTable.setParentScope(&parentScope);
+    cout << "Parent Scope ID: " << scopeTable.getParentScope()->getId() << endl; // Output: Parent Scope ID: 0  
+    cout << "Parent Scope Bucket Size: " << scopeTable.getParentScope()->getBucketSize() << endl; // Output: Parent Scope Bucket Size: 5
+    cout << "Parent Scope Scope Count: " << scopeTable.getParentScope()->getScopeCount() << endl; // Output: Parent Scope Scope Count: 0
+    cout << "Parent Scope Parent Scope: " << scopeTable.getParentScope()->getParentScope() << endl; // Output: Parent Scope Parent Scope: 0x0 (or nullptr)
+    cout << "Parent Scope ID: " << scopeTable.getParentScope()->getId() << endl; // Output: Parent Scope ID: 0
 
     return 0;
 }
