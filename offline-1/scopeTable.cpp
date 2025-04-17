afee0f63d32aa2000794c96c2085ec64f4762994
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2025-04-17 13:18:25 +0600

    symbol info tested succesfully

--- offline-1/scopeTable.cpp
+++ offline-1/scopeTable.cpp
@@ -1,2 +1,16 @@
 #include"symbolInfo.h"
 
+class ScopeTable
+{
+private:
+    int sid;
+    int bucketSize ;
+    int scopeCount ;
+    unsigned long (*hashFunction)(const std::string&); // function pointer
+    symbolInfo** table; // linked list of symbolInfo
+    ScopeTable* parentScope ; // pointer to parent scope, to be used as a stack
+public:
+
+
+
+};
--- offline-1/test.cpp
+++ offline-1/test.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World! hahahahaha" << endl;
-    cout<<haha()<<endl;
+    symbolInfo s1("x", "int");
+    symbolInfo s2("y", "float", &s1);
+    
+    cout << s1 << endl; // Output: Name: x, Type: int
+    cout << s2 << endl; // Output: Name: y, Type: float
+
+
+    cout << "Next Symbol of s2: " << *(s2.getNextSymbol()) << endl; // Output: Name: x, Type: int
+
+    s2.setName("z");
+    s2.setType("double");
+    cout << "After modification, s2: " << s2 << endl; // Output: Name: z, Type: double
+    cout << "Next Symbol of s2 after modification: " << *(s2.getNextSymbol()) << endl; // Output: Name: x, Type: int
+
     return 0;
 }
